25d448dcbbee0a38721220c2da8e8b2a25e44bc8
rafeeq-123 <rafeeq-123@hotmail.com>
2016-10-17 08:56:01 -0700

    basic controller for foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,44 @@
 class FoodsController < ApplicationController
+  before_action :find_food_id, only: [:show, :edit]
+
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    find_food_id
+    @food.update(food_params)
+    if @food.save
+       redirect_to @food
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    find_food_id
+    @food.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_food_id
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:dish, :food_groups)
+  end
+
 end
